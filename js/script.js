da3f34b16b622a25af275fe063b57ab3a81e3d2b
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-26 17:26:02 +0100

    init vue

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
-const items = [
-    {
-        title: 'Svezia',
-        image: 'img/01.jpg',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+const app = new Vue({
+    el: '#root',
+    data: {
+        items: [
+            {
+                title: 'Svezia',
+                image: 'img/01.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                title: 'Svizzera',
+                image: 'img/02.jpg',
+                text: 'Lorem ipsum'
+            },
+            {
+                title: 'Gran Bretagna',
+                title: 'img/03.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Germania',
+                title: 'img/04.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+            },
+            {
+                title: 'Paradise',
+                title: 'img/05.jpg',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+            }
+        ]
     },
-    {
-        title: 'Svizzera',
-        image: 'img/02.jpg',
-        text: 'Lorem ipsum'
-    },
-    {
-        title: 'Gran Bretagna',
-        title: 'img/03.jpg',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
-    },
-    {
-        title: 'Germania',
-        title: 'img/04.jpg',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
-    },
-    {
-        title: 'Paradise',
-        title: 'img/05.jpg',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+    methods: {
+        
     }
-];
+})
+
