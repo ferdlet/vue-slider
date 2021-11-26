9f4286b64c8b8eacdea815e39b3822e8a27bc9e0
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-26 17:23:14 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const items = [
+    {
+        title: 'Svezia',
+        image: 'img/01.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    },
+    {
+        title: 'Svizzera',
+        image: 'img/02.jpg',
+        text: 'Lorem ipsum'
+    },
+    {
+        title: 'Gran Bretagna',
+        title: 'img/03.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        title: 'Germania',
+        title: 'img/04.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+    },
+    {
+        title: 'Paradise',
+        title: 'img/05.jpg',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+    }
+];
